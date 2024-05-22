e4532b798cc65ae30f6dd361d7b37f4bdae349a6
Filip Leonard <leonard.filip@neotechnology.com>
2024-05-22 14:57:18 +0100

    Use printf

--- test.sh
+++ test.sh
@@ -21,20 +21,29 @@ make_body_2() {
 }
 
 make_body_3() {
-    src_pr=$(gh pr list --search "16127c1cea" --state merged --json 'title,body,url' --jq '.[0]')
-    src_pr_title=$(echo $src_pr | jq -r .title )
-    src_pr_body=$(echo $src_pr | jq -r .body )
-    src_pr_url=$(echo $src_pr | jq -r .url )
+    # src_pr=$(gh pr list --search "f72da1b85efafbbe89b95fb3a5cd36400484b36c" --state merged --json 'title,body,url' | jq -r '.[0]')
+    src_pr=$(gh pr list --search "f72da1b85efafbbe89b95fb3a5cd36400484b36c" --state merged --json 'title,body,url' --jq '.[0]')
+    # echo "${src_pr}"
     
+
+    # src_pr_title=$(gh pr list --search "f72da1b85efafbbe89b95fb3a5cd36400484b36c" --state merged --json 'title' --jq '.[0].title')
+    # src_pr_body=$(gh pr list --search "f72da1b85efafbbe89b95fb3a5cd36400484b36c" --state merged --json 'body' --jq '.[0].body')
+    # src_pr_url=$(gh pr list --search "f72da1b85efafbbe89b95fb3a5cd36400484b36c" --state merged --json 'url' --jq '.[0].url')
+    # printf "Original PR\n%s\n%s\n%s\n" "${src_pr_url}" "${src_pr_title}" "${src_pr_body}"
+
+    src_pr_title=$(printf "%s" "${src_pr}" | jq -r ".title" )
+    src_pr_body=$(printf "%s" "${src_pr}" | jq -r ".body" )
+    src_pr_url=$(printf "%s" "${src_pr}" | jq -r ".url" )
     printf "Original PR\n%s\n%s\n%s\n" "${src_pr_url}" "${src_pr_title}" "${src_pr_body}"
 
-        "$(cat << EOF
-$src_pr_url
 
->$src_pr_title
->$src_pr_body
-EOF
-)"
+#         "$(cat << EOF
+# $src_pr_url
+
+# >$src_pr_title
+# >$src_pr_body
+# EOF
+# )"
 }
 
 var="pull_request"
